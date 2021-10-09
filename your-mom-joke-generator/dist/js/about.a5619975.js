1ddc0376205718f842f8caa8af4dc37804a8630f
Alexander Bobo <alexanderbobo0@gmail.com>
2021-10-08 21:33:48 -0400

    build

---
+++ your-mom-joke-generator/dist/js/about.a5619975.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=n("7a23");const c={class:"about"},o=Object(s["d"])("h1",null,"Contact me if issues",-1),a=Object(s["d"])("h3",null," Contact me at buisness@alexdr.tech for issues. ",-1),u=[o,a];function b(t,e){return Object(s["o"])(),Object(s["c"])("div",c,u)}var i=n("6b0d"),r=n.n(i);const d={},l=r()(d,[["render",b]]);e["default"]=l}}]);
+//# sourceMappingURL=about.a5619975.js.map
