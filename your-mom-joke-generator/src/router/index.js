ae2188ccb8662d4effa720260a693d4c7bd12ac0
Alexander Bobo <53921135+alexdr0@users.noreply.github.com>
2022-03-26 23:11:56 +0100

    fixes to routing structure

--- your-mom-joke-generator/src/router/index.js
+++ your-mom-joke-generator/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Redirect from '../views/Redirect.vue'
+import About from '../views/About.vue'
 
 const routes = [
   {
@@ -12,16 +13,14 @@ const routes = [
     path: '/redirect',
     name: 'Redirect',
     component: Redirect
-  }
+  },
   {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    component: About
   },
   {
     path: "/redirect",
